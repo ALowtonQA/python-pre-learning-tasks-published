f9d0ebabb377c0c5924767ffa84feaff5ae81e8d
Anoush Lowton <ailowton@gmail.com>
2021-03-11 16:35:21 +0000

    pushing for validation

--- stretch-goals/calculator_stretch_goal.py
+++ stretch-goals/calculator_stretch_goal.py
@@ -1,8 +1,38 @@
+#This is my preferred solution, with a couple of alternatives included as comments.
 def calculator(a, b, operator):
-    # ==============
-    # Your code here
+    operation = "%s%s%s" % (a, operator, b)
+    calc = int(eval(operation))
+    binary = format(calc, 'b')
+    #binary = bin(calc).replace("0b", "")
+    #binary = "{0:b}".format(calc)
+    return int(binary)
 
-    # ==============
+#In case you were looking to see it done manually, I've included that below.
+def list_to_str(list1):
+    string_list = [str(value) for value in list1]
+    return "".join(string_list)
+    
+def convert_to_binary(dec):
+    binary = []
+    while dec > 0:
+        binary.append(dec % 2)
+        dec = int(dec/2)
+    binary.reverse()
+    return int(list_to_str(binary))
+
+def calculator2(a, b, operator):
+    if operator == "+":
+        calc = int(a + b)
+        return convert_to_binary(calc)
+    elif operator == "-":
+        calc = int(a - b)
+        return convert_to_binary(calc)
+    elif operator == "*":
+        calc = int(a * b)
+        return convert_to_binary(calc)
+    elif operator == "/":
+        calc = int(a / b)
+        return convert_to_binary(calc)
 
 print(calculator(2, 4, "+")) # Should print 110 to the console
 print(calculator(10, 3, "-")) # Should print 111 to the console
--- tasks/calculator.py
+++ tasks/calculator.py
@@ -1,8 +1,19 @@
+#Opted for use of eval() as it's significantly less code than selecting the operation conditionally.
+#I'm aware of the security risks of eval() but in this context it's fine.
 def calculator(a, b, operator):
-    # ==============
-    # Your code here
+    operation = "%s%s%s" % (a, operator, b)
+    return int(eval(operation))
 
-    # ==============
+# In case you were looking to see it done manually, I've included that below.
+def calculator2(a, b, operator):
+    if operator == "+":
+        return int(a + b)
+    elif operator == "-":
+        return int(a - b)
+    elif operator == "*":
+        return int(a * b)
+    elif operator == "/":
+        return int(a / b)
 
 print(calculator(2, 4, "+")) # Should print 6 to the console
 print(calculator(10, 3, "-")) # Should print 7 to the console
